9d04e5d6fd59ba780e389de81dfb606372fe0f4f
Deomap <deomapps@gmail.com>
2020-07-22 13:11:05 +0300

    db

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+import os
+from models import db, Books
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request, session, g, redirect, abort
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///first.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    pass
 
 
 if __name__ == '__main__':

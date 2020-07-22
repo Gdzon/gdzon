553cc18482758e12db072138e18522b8bb017b1b
Deomap <deomapps@gmail.com>
2020-07-22 13:40:41 +0300

    db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from models import db, Books
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, request, session, g, redirect, abort
@@ -11,8 +11,20 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    pass
+    return "hello!"
+
+
+@app.route("/<grade>/<subject>")
+def table_to_books(grade, subject):
+    if re.match(r"\d[0,1]?-klass", grade) and subject in subjects:
+        return "hello!"    # render_template('.html', grade=grade, subject=subject)
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
+subjects = ["matematika", "russkij_yazik"]

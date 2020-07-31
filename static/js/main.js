1e68eb4227754745edcbe34405626a0d65f9c7ee
Deomap <deomapps@gmail.com>
2020-07-31 22:26:14 +0300

    normally working navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $("ul.nav > li").click(
         function () {
-            $(this).find("ul").show("di");
-            $(this).find("ul").css("display", "table-row-group")
+            $(this).find("ul").slideToggle();
         }
     )
 })
+
